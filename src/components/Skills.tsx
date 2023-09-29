b91e29d8918c49ed25f0d9e276e354d22c3145fe
philouLeF <delcroixphilippe73@gmail.com>
2023-09-29 21:29:41 +0200

    ajout skillsµ

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,30 +1,59 @@
 import SkillsCard from "./SkillsCard";
 import HTML from "../assets/logo/html.svg";
 import CSS from "../assets/logo/css.svg";
+import Tailwind from "../assets/logo/tailwind.svg";
+import SASS from "../assets/logo/sass.svg";
 import JavaScript from "../assets/logo/javascript.svg";
+import TypeScript from "../assets/logo/typescript.svg";
+import React from "../assets/logo/react.svg";
 import InDesign from "../assets/logo/indesign.svg";
 import Illustrator from "../assets/logo/illustrator.svg";
 import Photoshop from "../assets/logo/photoshop.svg";
+import Figma from "../assets/logo/figma.svg";
 
 const Skills = () => {
   const webSkills = [
     {
       name: "HTML",
       icon: HTML,
-      mainColor: "rgb(227, 76, 38)",
-      accentColor: "rgb(240, 101, 41)",
+      mainColor: "rgb(240, 101, 41)",
+      accentColor: "rgb(227, 76, 38)",
     },
     {
       name: "CSS",
       icon: CSS,
-      mainColor: "rgb(38, 77, 228)",
-      accentColor: "rgb(41, 101, 241)",
+      mainColor: "rgb(41, 101, 241)",
+      accentColor: "rgb(38, 77, 228)",
+    },
+    {
+      name: "Tailwind",
+      icon: Tailwind,
+      mainColor: "rgb(53, 190, 248)",
+      accentColor: "rgb(255, 255, 255)",
+    },
+    {
+      name: "SASS",
+      icon: SASS,
+      mainColor: "rgb(206, 103, 154)",
+      accentColor: "rgb(255, 255, 255)",
     },
     {
       name: "JavaScript",
       icon: JavaScript,
       mainColor: "rgb(247, 223, 30)",
       accentColor: "rgb(0, 0, 0)",
+    },
+    {
+      name: "TypeScript",
+      icon: TypeScript,
+      mainColor: "rgb(45, 121, 199)",
+      accentColor: "rgb(255, 255, 255)",
+    },
+    {
+      name: "React",
+      icon: React,
+      mainColor: "rgb(97, 218, 251)",
+      accentColor: "rgb(255, 255, 255)",
     },
   ];
 
@@ -45,6 +74,12 @@ const Skills = () => {
       name: "Illustrator",
       icon: Illustrator,
       mainColor: "rgb(254, 153, 0)",
+      accentColor: "rgb(51, 0, 0)",
+    },
+    {
+      name: "Figma",
+      icon: Figma,
+      mainColor: "rgb(30, 30, 30)",
       accentColor: "rgb(51, 0, 0)",
     },
   ];
