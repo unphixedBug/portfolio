7d1c8965227c06c3d16976e1cfb60f98138c7553
philouLeF <delcroixphilippe73@gmail.com>
2023-10-01 22:14:58 +0200

    test chemin assets

--- src/components/Profil.tsx
+++ src/components/Profil.tsx
@@ -1,6 +1,6 @@
 const Profil = () => {
   return (
-    <div className="flex flex-col justify-around items-left  lg:px-56 lg:pt-32 pt-32 p-5 text-bleu-cv">
+    <div className="flex flex-col justify-around items-center lg:px-56 lg:pt-32 pt-32 p-5 text-bleu-cv">
       <p className="text-xl md:text-3xl lg:text-4xl">Hello ! Moi c'est</p>
       <h1 className="text-3xl md:text-5xl lg:text-6xl my-1 lg:my-3 font-bold">
         Philippe Delcroix
@@ -9,9 +9,9 @@ const Profil = () => {
         Développeur React junior
       </p>
       <a
-        href="public\cvWebDev2.pdf"
+        href="\portfolio\src\assets\cvWebDev2.pdf"
         target="_blank"
-        className="text-xs md:text-sm lg:text-base my-7 lg:my-14 inline-flex p-1 md:p-2 rounded-lg border border-bleu-cv text-bleu-cv hover:text-custom-bg  hover:bg-bleu-cv transition duration-300"
+        className="text-xs md:text-sm lg:text-base my-7 lg:my-14 inline-block w-auto p-1 md:p-2 rounded-lg border border-bleu-cv text-bleu-cv hover:text-custom-bg  hover:bg-bleu-cv transition duration-300"
       >
         Pour voir mon CV, ça se passe ici
       </a>
